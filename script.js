0bfd01d96a179e15c7af4d61e68f02b7506af4ff
Jina <jinayoussef@hotmail.co.uk>
2023-01-30 01:21:50 +0000

    added a filtered search

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import pokemonArray from "./data/pokemon.js";
 const cardContainer = document.querySelector(".card-container")
+const input = document.querySelector("#search-bar")
+const types = document.querySelector(".types")
+
 
 const capitaliseFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -12,7 +15,7 @@ const makeCard = (pokemonObject) => {
         
         <div class = "card__content">
             <h1 class = "card__heading">${capitaliseFirstLetter(pokemonObject.name)}</h1>
-            <p class = "card__text">${capitaliseFirstLetter(pokemonObject.name)} (#${pokemonObject.id}) is a ${pokemonObject.types.join("&")} type pokemon.</p>
+            <p class = "card__text">${capitaliseFirstLetter(pokemonObject.name)} (#${pokemonObject.id}) is a ${pokemonObject.types.join(" & ")} type pokemon.</p>
         </div>
     </div>`
 return cardHTML
@@ -22,3 +25,43 @@ const populatePage = (pokeArray) => {
     cardContainer.innerHTML = pokeArray.map(pokemon => makeCard(pokemon)).join("")
 }
 populatePage(pokemonArray)
+
+const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase()
+    const filteredArray = pokemonArray.filter(pokemon => {
+        if(pokemon.name.toLowerCase().includes(searchTerm)) {
+            return true;
+        } else{
+            return false;
+        }
+    })
+    populatePage(filteredArray)
+}
+
+const createTypesCheckboxes = () => {
+    const array = []
+    for (let index = 0; index < pokemonArray.length; index++) {
+        if (pokemonArray[index].types.length >= 2) {
+            array.push(pokemonArray[index].types[0].toString())
+            array.push(pokemonArray[index].types[1].toString())
+        } else {
+            array.push(pokemonArray[index].types.toString())
+        }
+    }
+    const typesArray = [...new Set(array)]
+
+    for (let index = 0; index < typesArray.length; index++) {
+        const typesHTML =
+        `<div class="type">
+        <label for="${typesArray[index]}">${capitaliseFirstLetter(typesArray[index])}</label>
+        <input type="checkbox" id="${typesArray[index]}">
+        </div>`  
+        types.innerHTML += typesHTML
+    }
+}
+
+createTypesCheckboxes()
+
+
+
+input.addEventListener("input", handleSearch)
